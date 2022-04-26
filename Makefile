6e19e55ac79b77cd1e66e8cf61e763749e9c4cb4
C. R. Van West <charles@vanwestco.com>
2022-04-25 22:07:38 -0400

    Add reference sort

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 reference: provided.cpp reference.cpp
-	c++ -D REFERENCE -o reference provided.cpp
+	c++ -g -D REFERENCE -o reference provided.cpp
 
 mine: provided.cpp mine.cpp
-	c++ -o mine provided.cpp
+	c++ -g -o mine provided.cpp
 
 .PHONY: test
 test: reference mine
--- reference.cpp
+++ reference.cpp
@@ -3,12 +3,36 @@
 constexpr const size_t maximum_items = 1'010'000; // one percent over a million
 Data* entries[maximum_items];
 
+bool are_ordered(Data* first, Data* second) {
+	const int last_name_comp = first->lastName.compare(second->lastName);
+	const int first_name_comp = first->firstName.compare(second->firstName);
+	const int ssn_comp = first->ssn.compare(second->ssn);
+
+	if (last_name_comp < 0) { // ordered by last name
+		return true;
+	} else if (last_name_comp == 0) { // last names equal
+		if (first_name_comp < 0) { // ordered by first name
+			return true;
+		} else if (first_name_comp == 0) { // first names equal
+			if (ssn_comp < 0) { // ordered enough by ssns
+				return true;
+			}
+		}
+	}
+
+	return false; // not ordered yet, or equal
+}
+
 void sortDataList(list<Data *> &l) {
+	const size_t length = l.size();
+
 	size_t index = 0;
 	for (auto iter = l.begin(); iter != l.end(); ++iter)
 		entries[index++] = *iter;
 
-	// reverse list for a lark
+	sort(entries, entries + length, are_ordered);
+
+	index = 0;
 	for (auto iter = l.begin(); iter != l.end(); ++iter)
-		*iter = entries[--index];
+		*iter = entries[index++];
 }
