cfd80eff5d89ca2b611bc2821ceb3bc5ada6b41a
C. R. Van West <charles@vanwestco.com>
2022-04-25 20:22:01 -0400

    Test architecture so far in a dumb way

--- reference.cpp
+++ reference.cpp
@@ -1,1 +1,14 @@
-void sortDataList(list<Data *> &l) { }
+#include <algorithm>
+
+constexpr const size_t maximum_items = 1'010'000; // one percent over a million
+Data* entries[maximum_items];
+
+void sortDataList(list<Data *> &l) {
+	size_t index = 0;
+	for (auto iter = l.begin(); iter != l.end(); ++iter)
+		entries[index++] = *iter;
+
+	// reverse list for a lark
+	for (auto iter = l.begin(); iter != l.end(); ++iter)
+		*iter = entries[--index];
+}
