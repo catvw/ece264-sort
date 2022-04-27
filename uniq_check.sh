263de4fd6fa4954c8e294a130db37e90a1f349d0
C. R. Van West <charles@vanwestco.com>
2022-04-26 22:52:42 -0400

    Fix bug in uniq_check.sh

--- uniq_check.sh
+++ uniq_check.sh
@@ -2,7 +2,7 @@
 file=$1
 chars=$2
 
-repeats=`uniq $file | sed -E -e "s/^(.{,$chars}).*$/\\1/" | uniq -d`
+repeats=`sort $file | uniq | sed -E -e "s/^(.{,$chars}).*$/\\1/" | uniq -d`
 
 for rep in $repeats; do
 	grep ^$rep $file
