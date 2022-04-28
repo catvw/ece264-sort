4c968981e4883ec257d408a08afc73dd98fd82fb
C. R. Van West <charles@vanwestco.com>
2022-04-28 18:21:06 -0400

    Add LICENSE.md, README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+#### Since you asked...
+
+Copyright (C) 2022, C. R. Van West.
+
+Distributed under [GPLv3](LICENSE.md) or later.
--- mine.cpp
+++ mine.cpp
@@ -1,3 +1,7 @@
+/*
+ * mine.cpp - terribly named; sorts four datasets based on name and ssn
+ */
+
 #include <cstdint>
 #include <cstring>
 
@@ -238,3 +242,20 @@ void sortDataList(list<Data *> &l) {
 	for (auto iter = l.begin(); iter != l.end(); ++iter)
 		*iter = entries[index++].data;
 }
+
+/*
+ * Copyright (C) 2022, C. R. Van West
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
